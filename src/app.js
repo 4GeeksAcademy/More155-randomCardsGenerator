e59f7ba33408ce7e61d78dd5adb56c892c3d8050
Morena <morenicora@gmail.com>
2026-01-10 00:22:35 +0000

    timer added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
@@ -15,10 +13,13 @@ window.onload = function() {
 
   const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
+  let secondsLeft = 10;
+  const countdownElement = document.getElementById("countdown");
+
   function drawRandomCard() {
     const randomSuit = suits[Math.floor(Math.random() * suits.length)];
     const randomValue = values[Math.floor(Math.random() * values.length)];
-
+    
     const card = document.getElementById("card");
     const topLeft = card.querySelector(".top-left");
     const center = card.querySelector(".center");
@@ -26,11 +27,27 @@ window.onload = function() {
 
     card.classList.remove("heart", "diamond", "spade", "club");
     card.classList.add(randomSuit.name);
-
+    
     topLeft.textContent = randomSuit.symbol;
     bottomRight.textContent = randomSuit.symbol;
     center.textContent = randomValue;
+
+    secondsLeft = 10;
+    if (countdownElement) countdownElement.textContent = secondsLeft;
   }
+
+  function updateCountdown() {
+    secondsLeft--;
+    if (countdownElement) {
+      countdownElement.textContent = secondsLeft;
+    }
+    if (secondsLeft <= 0) {
+      secondsLeft = 10;
+    }
+  }
+
+  setInterval(updateCountdown, 1000);
+  setInterval(drawRandomCard, 10000);
 
   drawRandomCard();
 
